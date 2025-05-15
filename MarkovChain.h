6e312c674d0bff8bc4249c8adfa57ab1cd8d62fb
yassienE4 <yassienebrahim@gmail.com>
2025-05-16 00:14:49 +0300

    .

--- MarkovChain.h
+++ MarkovChain.h
@@ -12,12 +12,19 @@ using Matrix = vector<vector<double>>;
 
 class MarkovChain 
 {
-  public:
+public:
     MarkovChain(Matrix transitionMatrix, vector<double> initialState);
     vector<double> step(); // perform a step
     void simulate(int steps); // simulate n steps
     void printCurrentState();
-  private:
+
+    // if we think we need more we can add a function that detects if
+    // the distribution converges to a stationary distribution
+    // basically after each step we compare the difference between them and if
+    // it is small enough then we can say it converged
+    // bool hasConverged();
+    
+private:
     Matrix P;
     vector<double> state;
 };
